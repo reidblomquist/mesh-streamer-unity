c9637b39aa6f31aab68ca2045a1cff767173d6dd
Reid Blomquist <reid.blomquist@gmail.com>
2016-06-09 21:33:29 -0700

    init

---
+++ Assets/Scripts/MeshSerializer.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Collections;
+
+public class MeshSerializer : MonoBehaviour {
+
+    private static readonly byte[] marker = Encoding.ASCII.GetBytes("MESHDATA");
+
+	public byte[] Serialize(Mesh mesh)
+	{
+		int vertexCount = mesh.vertexCount;
+
+		int positionCount = vertexCount; // x, y, z tripplets
+		int colorCount = vertexCount; // r, g, b tripplets
+		int indexCount = vertexCount;
+
+		int headerDataByteCount = 16;
+		int positionDataByteCount = positionCount * 3 * 4;
+		int colorDataByteCount = colorCount * 3;
+		int indexDataByteCount = indexCount * 3 * 2;
+
+		int packetSize = headerDataByteCount + positionDataByteCount + colorDataByteCount + indexDataByteCount;
+
+		byte[] packet = new byte[packetSize];
+		Array.Copy(marker, 0, packet, 0, marker.Length);
+
+		int endOfMarker = marker.Length;
+		putInt16(packet, endOfMarker, positionCount);
+		putInt16(packet, endOfMarker + 2, colorCount);
+		putInt16(packet, endOfMarker + 4, indexCount / 3);
+
+		int positionDataStart = 16;
+		int colorDataStart = positionDataStart + positionDataByteCount;
+		int indexDataStart = colorDataStart + colorDataByteCount;
+
+		for (int i = 0; i < mesh.vertexCount; i++)
+		{ //<>//
+
+			Vector3 position = mesh.vertices[i];
+			Color vertexColor = mesh.colors[i];
+
+			// encode the position data
+			putFloat(packet, positionDataStart + (i * 12), position.x);
+			putFloat(packet, positionDataStart + (i * 12) + 4, position.y);
+			putFloat(packet, positionDataStart + (i * 12) + 8, position.z);
+
+			byte r = (byte)(vertexColor.r);
+			byte g = (byte)(vertexColor.g);
+			byte b = (byte)(vertexColor.b);
+
+			packet[colorDataStart + (i * 3)] = r;
+			packet[colorDataStart + (i * 3) + 1] = g;
+			packet[colorDataStart + (i * 3) + 2] = b;
+
+			putInt16(packet, indexDataStart + (i * 2), i);
+		}
+		return packet;
+	}
+
+	void putInt16(byte[] destination, int offset, int value)
+	{
+		byte firstByte = (byte)(value & 0xff);
+		byte secondByte = (byte)((value >> 8) & 0xff);
+
+		destination[offset] = firstByte;
+		destination[offset + 1] = secondByte;
+	}
+
+	void putFloat(byte[] destination, int offset, float value)
+	{
+
+		byte[] bytes = BitConverter.GetBytes(value);
+
+		destination[offset] = bytes[0];
+		destination[offset + 1] = bytes[1];
+		destination[offset + 2] = bytes[2];
+		destination[offset + 3] = bytes[3];
+	}
+}
---
+++ Assets/Scripts/MeshStreaming.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MeshStreaming : MonoBehaviour {
+
+    public int gridSize = 10;
+    float scale = 1.0f;
+    float noiseScale = 0.5f;
+
+	private Mesh mesh;
+	private Vector3[] vertices;
+	private Color[] colors;
+	private MeshSerializer serializer;
+
+	private void Start () {
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		serializer = new MeshSerializer();
+		mesh.name = "Some Rad Meshy Thing";
+
+		StartCoroutine(Generate());
+	}
+
+	private IEnumerator Generate () {
+		float timeScale = Time.deltaTime;
+
+		vertices = new Vector3[(gridSize + 1) * (gridSize + 1)];
+		colors = new Color[vertices.Length];
+		Vector2[] uv = new Vector2[vertices.Length];
+		Vector4[] tangents = new Vector4[vertices.Length];
+		Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+		for (int i = 0, y = 0; y <= gridSize; y++) {
+			for (int x = 0; x <= gridSize; x++, i++) {
+				colors[i] = new Color(x / gridSize * 255, y / gridSize * 255, 128);
+				vertices[i] = new Vector3((x * scale), Mathf.PerlinNoise((x * noiseScale), (y * timeScale)) * scale, y * scale);
+				uv[i] = new Vector2((float)x / gridSize, (float)y / gridSize);
+				tangents[i] = tangent;
+			}
+		}
+		mesh.Clear();
+		mesh.vertices = vertices;
+		mesh.colors = colors;
+
+		int[] triangles = new int[gridSize * gridSize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < gridSize; y++, vi++) {
+			for (int x = 0; x < gridSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + gridSize + 1;
+				triangles[ti + 5] = vi + gridSize + 2;
+			}
+		}
+		mesh.triangles = triangles;
+		serializer.Serialize(mesh);
+
+		yield return null;
+	}
+}
